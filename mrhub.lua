a0c25ecd07e4f485bf73dfc0d9fc2253b67be08c
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-03-04 04:11:22 -0500

    Update mrhub.lua

--- mrhub.lua
+++ mrhub.lua
@@ -1,1 +1,5 @@
+local library = loadstring(game:HttpGet("https://github.com/GoHamza/AppleLibrary/blob/main/main.lua?raw=true"))()
 
+local window = library:init("Titlebar", true, Enum.KeyCode.RightShift, true)
+
+local window = library:init(titleText: string, splash: boolean, showHideKeybind: KeyCode, deletePreviousUI: boolean)
